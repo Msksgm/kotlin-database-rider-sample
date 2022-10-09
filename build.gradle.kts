bbb2f0238bec6140b804d88d9d20b10bf786a044
Msksgm <msksgm3124@gmail.com>
2022-10-09 11:42:21 +0900

    chore: database rider を導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,24 +28,46 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    /**
+     * Database Rider
+     *
+     * - Rider Core
+     * - Rider Spring
+     * - Rider JUnit 5
+     *
+     * URL
+     * - https://database-rider.github.io/database-rider/
+     * MavenCentral
+     * - https://mvnrepository.com/artifact/com.github.database-rider/rider-core
+     * - https://mvnrepository.com/artifact/com.github.database-rider/rider-spring
+     * - https://mvnrepository.com/artifact/com.github.database-rider/rider-junit5
+     * Main用途
+     * - JUnitでDB周りのテスト時のヘルパー
+     * 概要
+     * - テーブルの事前条件、事後条件を簡潔に設定できる
+     */
+    implementation("com.github.database-rider:rider-core:1.34.0")
+    implementation("com.github.database-rider:rider-spring:1.34.0")
+    testImplementation("com.github.database-rider:rider-junit5:1.34.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
